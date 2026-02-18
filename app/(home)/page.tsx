5c3ad940d445c497116aa54451412c19b29563b5
Elexvx Inc <elexvx@gmail.com>
2026-02-19 00:35:25 +0800

    del

--- app/(home)/page.tsx
+++
@@ -1,6 +0,0 @@
-import { homeConfig } from '@/config/home';
-import { HomeTemplate } from '@/components/home/home-template';
-
-export default function HomePage() {
-  return <HomeTemplate config={homeConfig} />;
-}
--- config/site.ts
+++
@@ -1,3 +0,0 @@
-export const siteLayout = {
-  containerClass: 'max-w-[1280px]',
-};
